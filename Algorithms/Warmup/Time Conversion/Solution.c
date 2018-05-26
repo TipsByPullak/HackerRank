40c0a897a06cbb13f4ce88b7eb84162c5ba6b375
Pullak Barik <39293478+TipsByPullak@users.noreply.github.com>
2018-05-26 17:28:52 +0530

    Updating Solution.c
    A shorter, much better solution.

--- Algorithms/Warmup/Time Conversion/Solution.c
+++ Algorithms/Warmup/Time Conversion/Solution.c
@@ -1,49 +1,22 @@
-//https://www.hackerrank.com/challenges/time-conversion
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
+//https://www.hackerrank.com/challenges/time-conversion/problem
+#include<stdio.h>
+#include<stdlib.h>
 
-char* timeConversion(char* s) {
-    if (s[8] == 'A'){
-        if(s[0]=='1' && s[1]=='2'){
-            s[0]='0';
-			s[1]='0';
-		}
-        s[8] = '\0';
-    }
-    else {
-		if(s[0]=='1' && s[1]=='2'){
-			s[8] = '\0';
-			return s;
-		}
-        char t[3];
-        t[0] = s[0];
-        t[1] = s[1];
-        t[2] = '\0';
-        int p;
-        p = atoi(t);
-        p = (p+12)%24;
-        int length = snprintf( NULL, 0, "%d", p );
-        char* tn = (char *)malloc( 3 );
-        snprintf( tn, length + 1, "%d", p );
-        s[0] = tn[0];
-        s[1] = tn[1];
-        s[8] = '\0';
-    }
-    return s;
+void timeConversion(char s[]) {
+    int hh, mm, ss;
+    hh = (s[0] - '0')*10 + (s[1] - '0');
+    mm = (s[3] - '0')*10 + (s[4] - '0');
+    ss = (s[6] - '0')*10 + (s[7] - '0');
+    if(s[8] == 'A' && hh == 12) hh = 0;
+    if(s[8] == 'P' && (1<=hh && hh<=11)) hh = hh + 12;
+    printf("%02d:%02d:%02d", hh, mm, ss);
 }
-
-int main() {
-    char* s = (char *)malloc(512000 * sizeof(char));
-    scanf("%s", s);
-    //int result_size;
-    char* result = timeConversion(s);
-    printf("%s\n", result);
-	//fflush(stdin);
-	//getchar();
+    
+int main()
+{
+    char s[10];
+    for(int i = 0; i < 10; ++i)
+      scanf(" %c", &s[i]);
+    timeConversion(s);
     return 0;
 }

cedc98ac9d0d5f37db73e1c3133a5e2bf943e6e4
Pullak Barik <39293478+TipsByPullak@users.noreply.github.com>
2018-05-26 17:05:37 +0530

    Created Solution.c
    My first attempt at the solution

---
+++ Algorithms/Warmup/A Very Big Sum/Solution.c
@@ -0,0 +1,28 @@
+//https://www.hackerrank.com/challenges/a-very-big-sum/problem
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+long long int aVeryBigSum(int n, int ar_size, long int* ar) {
+    long long int sum = 0;
+    for(int i=0; i < n; ++i){
+        sum = sum + ar[i];
+    }
+    return sum;
+}
+
+int main() {
+    int n; 
+    scanf("%i", &n);
+    long int *ar = malloc(sizeof(long int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       scanf("%li",&ar[ar_i]);
+    }
+    long int result = aVeryBigSum(n, n, ar);
+    printf("%lld\n", result);
+    return 0;
+}

cea9cb7beaaaf57801cfff02770eee4b6f0177d1
Gauravi97 <gauravi.kabadi@gmail.com>
2022-10-02 00:56:47 +0530

    add logic

--- Algorithms/Implementation/Flatland Space Stations/solution.cpp
+++ Algorithms/Implementation/Flatland Space Stations/solution.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 
+int global_city;
+
+void subtractGlobalCity(int &a)
+{
+    a = abs(a - global_city);
+}
+
 int furthest_space_station(int n, vector<int> cities)
 {
     int ans = 0;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        vector<int> dist = cities;
+        
+        global_city = i;
+        
+        for_each(cities.begin(), cities.end(), &subtractGlobalCity);
+        
+        auto curr_max_itr = max_element(cities.begin(), cities.end());
+        
+        if (*curr_max_itr > ans) ans = *curr_max_itr;
+    }
     
     return ans;
 }
@@ -21,7 +41,7 @@ int main()
         int temp;
         cin >> temp;
         
-        cities.push_back(temp);
+        cities.push_back(temp - 1);
     }
     
     cout << furthest_space_station(n, cities) << endl;

ef2a743589a59bf05286dbd4f3a648c4afa512c8
Gauravi97 <gauravi.kabadi@gmail.com>
2022-10-02 00:28:29 +0530

    init code in soln file

--- Algorithms/Implementation/Flatland Space Stations/solution.cpp
+++ Algorithms/Implementation/Flatland Space Stations/solution.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
+int furthest_space_station(int n, vector<int> cities)
+{
+    int ans = 0;
+    
+    return ans;
+}
+
 int main()
 {
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> cities;
+    
+    for(int i = 0; i < m; ++i)
+    {
+        int temp;
+        cin >> temp;
+        
+        cities.push_back(temp);
+    }
+    
+    cout << furthest_space_station(n, cities) << endl;
+    
     return 0;
 }

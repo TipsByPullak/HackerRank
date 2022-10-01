d2ef7c2dab9c10527bfba1477507ac481a052c5c
Gauravi97 <gauravi.kabadi@gmail.com>
2022-10-02 01:04:31 +0530

    use temp vector properly

--- Algorithms/Implementation/Flatland Space Stations/solution.cpp
+++ Algorithms/Implementation/Flatland Space Stations/solution.cpp
@@ -9,6 +9,17 @@ void subtractGlobalCity(int &a)
     a = abs(a - global_city);
 }
 
+void printVectorElement(int &a)
+{
+    cout << a << ", ";
+}
+
+void printVector(vector<int> v)
+{
+    for_each(v.begin(), v.end(), &printVectorElement);
+    cout << endl;
+}
+
 int furthest_space_station(int n, vector<int> cities)
 {
     int ans = 0;
@@ -16,12 +27,12 @@ int furthest_space_station(int n, vector<int> cities)
     for (int i = 0; i < n; ++i)
     {
         vector<int> dist = cities;
-        
+
         global_city = i;
         
-        for_each(cities.begin(), cities.end(), &subtractGlobalCity);
+        for_each(dist.begin(), dist.end(), &subtractGlobalCity);
         
-        auto curr_max_itr = min_element(cities.begin(), cities.end());
+        auto curr_max_itr = min_element(dist.begin(), dist.end());
         
         if (*curr_max_itr > ans) ans = *curr_max_itr;
     }
